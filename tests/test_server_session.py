2dfc75682f2b8eaf5e8f6cfdf1ccbf19947855ba
Tobias Schneider <schneider@blinkenlichts.net>
2014-03-16 04:32:59 +0100

    fix(server-session): update the tests for the server session

--- tests/test_server_session.py
+++ tests/test_server_session.py
@@ -3,6 +3,7 @@ import mock
 import logging
 import ConfigParser
 from  decimal import Decimal
+import sys
 
 import nupay
 
@@ -25,116 +26,125 @@ class SessionManagerTest(unittest.TestCase):
         self.config = db_config()
 
     def test_create_session(self):
-        self.session_manager = nupay.SessionManager(self.config)
-        with self.session_manager.create_session() as session:
-            self.assertIsInstance(session, nupay.Session)
+        self.session_manager = nupay.ServerSessionManager(self.config)
+        session = self.session_manager.create_session()
+        self.assertIsInstance(session, nupay.ServerSession)
+        session.close()
 
     def test_create_session_manager(self):
-        self.session_manager = nupay.SessionManager(self.config)
+        self.session_manager = nupay.ServerSessionManager(self.config)
 
     def test_create_bad_session_mamager(self):
         self.config.set("Database", "password", "fnord223")
-        self.assertRaises(nupay.SessionConnectionError, nupay.SessionManager, self.config)
+        self.assertRaises(nupay.SessionConnectionError, nupay.ServerSessionManager, self.config)
 
 class SessionTest(unittest.TestCase):
 
     def setUp(self):
-        #logging.basicConfig(level=logging.DEBUG)
-        logging.basicConfig(level=logging.ERROR)
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
+        #logging.basicConfig(level=logging.ERROR)
         self.config = db_config()
-        self.session_manager = nupay.SessionManager(self.config)
-        with self.session_manager.create_session() as session:
-            session.bootstrap_db()
+        self.session_manager = nupay.ServerSessionManager(self.config)
+        self.session_manager.bootstrap_db()
+        session = self.session_manager.create_session()
 
     def tearDown(self):
         pass
 
     def test_bad_validation(self):
-        tokens = [nupay.Token(), nupay.Token()]
-        with self.session_manager.create_session() as session:
-            session.add_tokens(tokens)
+        session = self.session_manager.create_session()
+        session.create_tokens(Decimal(5))
+        session.close()
  
         tokens = [nupay.Token(), nupay.Token()]
-        with self.session_manager.create_session() as session:
-            credit = session.validate_tokens(tokens)
-            self.assertEqual(credit, 0)
+        session = self.session_manager.create_session()
+        credit = session.validate_tokens(tokens)
+        self.assertEqual(credit, 0)
+        session.close()
     
-    def test_add_token(self):
-        tokens = [nupay.Token(), nupay.Token()]
-        with self.session_manager.create_session() as session:
-            session.add_tokens(tokens)
+    def test_create_token(self):
+        session = self.session_manager.create_session()
+        session.create_tokens(Decimal(5))
+        session.close()
  
     def test_good_validation(self):
-        tokens = [nupay.Token(), nupay.Token()]
-        with self.session_manager.create_session() as session:
-            session.add_tokens(tokens)
+        session = self.session_manager.create_session()
+        tokens = session.create_tokens(Decimal(1))
+        session.close()
         
-        with self.session_manager.create_session() as session:
-            credit = session.validate_tokens(tokens)
-            self.assertEqual(credit, Decimal('1'))
+        session = self.session_manager.create_session()
+        credit = session.validate_tokens(tokens)
+        self.assertEqual(credit, Decimal('1'))
+        session.close()
  
     def test_double_validation(self):
-        tokens = [nupay.Token(), nupay.Token()]
-        with self.session_manager.create_session() as session:
-            session.add_tokens(tokens)
+        session = self.session_manager.create_session()
+        tokens = session.create_tokens(Decimal(1))
+        session.close()
         
-        with self.session_manager.create_session() as session:
-            credit = session.validate_tokens(tokens)
-            self.assertEqual(credit, Decimal('1'))
+        session = self.session_manager.create_session()
+        credit = session.validate_tokens(tokens)
+        self.assertEqual(credit, Decimal('1'))
+        session.close()
  
-        with self.session_manager.create_session() as session:
-            credit = session.validate_tokens(tokens)
-            self.assertEqual(credit, Decimal('1'))
+        session = self.session_manager.create_session()
+        credit = session.validate_tokens(tokens)
+        self.assertEqual(credit, Decimal('1'))
+        session.close()
        
     def test_bad_cash(self):
-        tokens = [nupay.Token(), nupay.Token(), nupay.Token(), nupay.Token()]
-        with self.session_manager.create_session() as session:
-            session.add_tokens(tokens)
+        session = self.session_manager.create_session()
+        tokens = session.create_tokens(Decimal(2))
+        session.close()
 
-        with self.session_manager.create_session() as session:
-            session.validate_tokens(tokens)
-            self.assertRaises(nupay.NotEnoughCreditError, session.cash, Decimal(2.1))
-            self.assertEqual(0, session.total)
-            self.assertEqual(Decimal(2), session.credit)
+        session = self.session_manager.create_session()
+        session.validate_tokens(tokens)
+        self.assertRaises(nupay.NotEnoughCreditError, session.cash, Decimal(2.1))
+        self.assertEqual(0, session.total)
+        self.assertEqual(Decimal(2), session.credit)
+        session.close()
 
     def test_good_cash(self):
-        tokens = [nupay.Token(), nupay.Token(), nupay.Token(), nupay.Token()]
-        with self.session_manager.create_session() as session:
-            session.add_tokens(tokens)
+        session = self.session_manager.create_session()
+        tokens = session.create_tokens(Decimal(2))
+        session.close()
 
-        with self.session_manager.create_session() as session:
-            session.validate_tokens(tokens)
-            session.cash(Decimal(2.0))
-            self.assertEqual(Decimal(2.0), session.total)
-            self.assertEqual(Decimal(0), session.credit)
+        session = self.session_manager.create_session()
+        session.validate_tokens(tokens)
+        session.cash(Decimal(2.0))
+        self.assertEqual(Decimal(2.0), session.total)
+        self.assertEqual(Decimal(0), session.credit)
+        session.close()
 
     def test_good_cash_split(self):
-        tokens = [nupay.Token(), nupay.Token(), nupay.Token(), nupay.Token()]
-        with self.session_manager.create_session() as session:
-            session.add_tokens(tokens)
+        session = self.session_manager.create_session()
+        tokens = session.create_tokens(Decimal(2))
+        session.close()
 
-        with self.session_manager.create_session() as session:
-            session.validate_tokens(tokens)
-            session.cash(Decimal(1.0))
-            session.cash(Decimal(0.25))
-            self.assertRaises(nupay.NotEnoughCreditError, session.cash, Decimal(1.0))
-            self.assertEqual(Decimal(1.5), session.total)
-            self.assertEqual(Decimal(0.5), session.credit)
+        session = self.session_manager.create_session()
+        session.validate_tokens(tokens)
+        session.cash(Decimal(1.0))
+        session.cash(Decimal(0.25))
+        self.assertRaises(nupay.NotEnoughCreditError, session.cash, Decimal(1.0))
+        self.assertEqual(Decimal(1.5), session.total)
+        self.assertEqual(Decimal(0.5), session.credit)
+        session.close()
 
     def test_rollback(self):
-        tokens = [nupay.Token(), nupay.Token(), nupay.Token(), nupay.Token()]
-        with self.session_manager.create_session() as session:
-            session.add_tokens(tokens)
+        session = self.session_manager.create_session()
+        tokens = session.create_tokens(Decimal(2))
+        session.close()
 
-        with self.session_manager.create_session() as session:
-            session.validate_tokens(tokens)
-            session.cash(Decimal(1.0))
-            session.cash(Decimal(0.25))
-            session.rollback()
-            self.assertRaises(nupay.RollbackError, session.rollback)
-            session.cash(Decimal(1.0))
-            self.assertRaises(nupay.NotEnoughCreditError, session.cash, Decimal(1.0))
-            self.assertEqual(Decimal(2.0), session.total)
+        session = self.session_manager.create_session()
+        session.validate_tokens(tokens)
+        session.cash(Decimal(1.0))
+        tokens = (session.cash(Decimal(0.25)))
+        session.rollback(tokens)
+        self.assertRaises(nupay.RollbackError, session.rollback, tokens)
+        session.cash(Decimal(1.0))
+        self.assertRaises(nupay.NotEnoughCreditError, session.cash, Decimal(1.0))
+        self.assertEqual(Decimal(2.0), session.total)
+        session.close()
 
 if __name__ == '__main__':
     unittest.main()
