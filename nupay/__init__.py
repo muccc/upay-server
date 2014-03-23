554a3ec23dd9e19666721373a8e75cd92ee58d39
Tobias Schneider <schneider@blinkenlichts.net>
2014-03-23 16:22:30 +0100

    chore(upay): remove old files

--- nupay/__init__.py
+++ nupay/__init__.py
@@ -1,8 +1,6 @@
 from token_reader import USBTokenReader, NoTokensAvailableError, read_tokens_from_file
 from token import BadTokenFormatError, Token
 from session import SessionManager, Session, SessionConnectionError, NotEnoughCreditError, RollbackError, CashTimeoutError, SessionError
-from server_session import ServerSessionManager, ServerSession
-from server_usermanager import ServerUserManager
 from token_authority import TokenAuthority, NoValidTokenFoundError, TimeoutError
 from token_client import TokenClient
 from token_collector import MQTTCollector
