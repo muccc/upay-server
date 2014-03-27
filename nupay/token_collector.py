598c95bd475086d108c5e3acac0b4421552efd0b
Tobias Schneider <schneider@blinkenlichts.net>
2014-03-27 21:19:42 +0100

    feat(mqttcollector): try to reconnect to the broker

--- nupay/token_collector.py
+++ nupay/token_collector.py
@@ -1,6 +1,7 @@
 import mosquitto
 import threading
 import Queue
+import time
 
 class Collector(object):
     def __init__(self):
@@ -13,12 +14,24 @@ class Collector(object):
 class MQTTCollector(Collector):
     def __init__(self, server, topic, client_id):
         self._client = mosquitto.Mosquitto(client_id)
-        self._client.connect(server)
         self._mqtt_thread = threading.Thread(target = self._run)
         self._keep_running = True
         self._topic = topic
+        self._server = server
         self._token_queue = Queue.Queue()
+        self._connect()
         self._mqtt_thread.start()
+
+    def _connect(self):
+        self._connected = False
+        while self._connected == False:
+            try:
+                rc = self._client.connect(self._server)
+                if rc == 0:
+                    self._connected = True
+            except Exception as e:
+                print e
+            time.sleep(1)
 
     def stop(self):
         self._keep_running = False
@@ -27,7 +40,9 @@ class MQTTCollector(Collector):
 
     def _run(self):
         while self._keep_running:
-            self._client.loop()
+            rc = self._client.loop()
+            if rc != 0:
+                self._connect()
             try:
                 token = self._token_queue.get(block = False)
                 self._client.publish(self._topic, str(token), 2)
@@ -37,3 +52,8 @@ class MQTTCollector(Collector):
     def collect_tokens(self, tokens):
         Collector.collect_tokens(self, tokens)
         map(self._token_queue.put, tokens)
+
+    @property
+    def connected(self):
+        return self._connected
+
