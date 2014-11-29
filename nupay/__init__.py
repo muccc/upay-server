4f5db56983d53ed0b5445b7c38c82b0e81490983
Tobias Schneider <schneider@blinkenlichts.net>
2014-11-29 22:02:33 +0100

    chore(upay): Remove non server code

--- nupay/__init__.py
+++
@@ -1,6 +0,0 @@
-from token_reader import USBTokenReader, NoTokensAvailableError, read_tokens_from_file
-from token import BadTokenFormatError, Token
-from session import SessionManager, Session, SessionConnectionError, NotEnoughCreditError, RollbackError, CashTimeoutError, SessionError
-from token_authority import TokenAuthority, NoValidTokenFoundError, TimeoutError
-from token_client import TokenClient
-from token_collector import MQTTCollector, GITCollector, MailCollector, MQTTTokenForwarder
