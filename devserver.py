2f6ddb7921d0b9ecd56ceedb7eff596c0a7a5f82
Bernd Stolle <bsx+ccc@0xcafec0.de>
2014-11-30 17:47:33 +0100

    chore(upay.server) add devserver script + config

---
+++ devserver.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+
+from upay.server import app
+from upay.server.token_authority import TokenAuthority
+
+app.config.from_pyfile(os.path.join(os.getcwd(), 'devserver.cfg'))
+
+if 'USE_SSL' in app.config and app.config['USE_SSL']:
+    from OpenSSL import SSL
+    context = SSL.Context(SSL.TLSv1_2_METHOD)
+    context.use_privatekey_file('test.key')
+    context.use_certificate_file('test.crt')
+
+authority = TokenAuthority(app.config)
+authority.bootstrap_db()
+
+app.run()
